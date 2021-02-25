7ad976830386c64d2bde9512cf77cf5979d61a3b
tyler belmonte <tylerbelmonte@gmail.com>
2021-02-24 19:05:22 -0600

    array practice

---
+++ com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String[] myStringArray = {"Hello", "World,", "what", "is", "up."};
+        System.out.println(myStringArray[0] + " " + myStringArray[1] + " " + myStringArray[2] + " " + myStringArray[3] + " " + myStringArray[4]);
+    }
+}
